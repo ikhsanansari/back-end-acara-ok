20e294c3b49c21e5d81777939bd9a4f04cfa5bbb
ikhsanansari <ikhsanansari11@gmail.com>
2025-07-28 14:11:53 +0700

    feat: init project

---
+++ src/routes/api.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import dummyController from "../controllers/dummy.controller";
+
+const router = express.Router();
+router.get('/dummy', dummyController.dummy);
+export default router;
